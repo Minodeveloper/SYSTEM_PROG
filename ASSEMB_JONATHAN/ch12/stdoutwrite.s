1e128f87e5432ee89385714e5ee5beae52302c81
Minodeveloper <minoshideveloper8@gmail.com>
2023-01-15 22:24:58 +0530

    finished ch 12

---
+++ ASSEMB_JONATHAN/ch12/stdoutwrite.s
@@ -0,0 +1,39 @@
+.globl main
+
+.section .data
+
+formatstring1:
+	.ascii "The age of %s is %d.\n\0"
+sallyname:
+	.ascii "Sally\0"
+sallyage:
+	.quad 53
+formatstring2:
+	.ascii "%d and %d are %s's fovourite number numbers.\n\0"
+joshname:
+	.ascii "Josh\0"
+joshfavouritefirst:
+	.quad 7
+joshfavouritesecond:
+	.quad 13
+
+.section .text
+
+main:	
+	movq stdout, %rdi
+	movq $formatstring1, %rsi
+	movq $sallyname, %rdx
+	movq sallyage, %rcx
+	movq $0, %rax
+	call fprintf
+
+	movq stdout, %rdi
+	movq $formatstring2, %rsi
+	movq joshfavouritefirst, %rdx
+	movq joshfavouritesecond, %rcx
+	movq $joshname, %r8
+	movq $0, %rax
+	call fprintf
+
+	movq $0, %rax
+	ret
