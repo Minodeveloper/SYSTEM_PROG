e7b55d4290b992551c61cf1f5316ecb6f06d9191
Minodeveloper <minoshideveloper8@gmail.com>
2023-03-05 13:08:42 +0530

    contunued

--- JEFF_ASSEMBLY/prac/sandbox.asm
+++ JEFF_ASSEMBLY/prac/sandbox.asm
@@ -2,8 +2,9 @@
 
 SECTION .data
 
-roll	dq 0x64
-name	db "M"
+roll	db 0b10101111
+name	dq 0b11111111000011110011001101010101
+sent	dq "abcd",10
 	
 SECTION .bss
 
@@ -15,8 +16,9 @@ global _start
 _start:
 	nop
 
-	mov rax, roll
-	mov rbx, [rax]
-	
+	mov rax, name
+	;mov rbx, [roll]
+	mov rcx, [name]
+	;mov rdx, [sent]
 	nop
 
