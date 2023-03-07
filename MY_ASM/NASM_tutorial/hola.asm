f230b2f284fe93b7968d58447017ca1b522aa7e9
Minodeveloper <minoshideveloper8@gmail.com>
2023-03-08 00:26:13 +0530

    solved issue in hola.asm #need to learn sys v ABI

--- MY_ASM/NASM_tutorial/hola.asm
+++ MY_ASM/NASM_tutorial/hola.asm
@@ -1,13 +1,18 @@
+; ----------------------------------------------------------------------------------------
+; Writes "Hola, mundo" to the console using a C library. Runs on Linux.
+;
+;     nasm -felf64 hola.asm && gcc hola.o && ./a.out
+; ----------------------------------------------------------------------------------------
+message:
+          db        "Hola, mundo", 0        ; Not strings must be terminated with 0 in C
 
-	global	main
-	extern	puts
+          global    main
+          extern    puts
 
-SECTION	.text
-
-main:
-	mov	rdi, message
-	call	puts
-	ret
-
-message:	
-	db	"Hola, mundo",10
+          section   .text
+main:   
+	push	rdi
+	mov	rdi, message            
+       	call	puts                    
+       	pop 	rdi
+       	ret                              
