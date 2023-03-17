fdf740a68eeab9d5e46b38107f066beb20362f3c
Minodeveloper <minoshideveloper8@gmail.com>
2023-03-17 14:07:54 +0530

    ----

--- MY_ASM/JOGENSEN_x86/sumsquare.asm
+++ MY_ASM/JOGENSEN_x86/sumsquare.asm
@@ -9,4 +9,16 @@ SECTION	.text
 global _start
 
 _start:
-	mov	
+	mov	rcx,10
+	
+_add_:	mov	rax,rcx
+	mul	rax
+	add	qword[sum],rax
+	dec	rcx
+	cmp	rcx,0
+	jne	_add_
+	
+
+	mov	rax,60
+	mov	rdi,qword[sum]
+	syscall
