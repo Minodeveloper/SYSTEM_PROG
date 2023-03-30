bfaa98961a006c33b07d1a0ba67feb1de5511762
Minodeveloper <minoshideveloper8@gmail.com>
2023-03-31 00:52:27 +0530

    added comments

--- MY_ASM/JOGENSEN_x86/restart.asm
+++ MY_ASM/JOGENSEN_x86/restart.asm
@@ -1,3 +1,7 @@
+;syscall number 0xa9
+;link https://chromium.googlesource.com/chromiumos/docs/+/HEAD/constants/syscalls.md#x86-32_bit
+;https://man7.org/linux/man-pages/man2/reboot.2.html
+
 section	.data
 
 section	.text
@@ -6,8 +10,8 @@ global _start
 
 _start:
 	mov	rax,0xa9
-	mov	rdi,0xfee1dead
-	mov	rsi,672274793
-	mov	rdx, 0x4321fedc
-	mov	r10,0
+	mov	rdi,0xfee1dead	;magic number 
+	mov	rsi,672274793	;magic number2
+	mov	rdx, 0x4321fedc	;command
+	mov	r10,0		;don't know
 	syscall
