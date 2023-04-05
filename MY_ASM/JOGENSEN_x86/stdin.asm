3dd273ae4bd8cbe48bedd5ba414ff696e0ea576e
Minodeveloper <minoshideveloper8@gmail.com>
2023-04-06 00:11:41 +0530

    syscall stdin stdout

--- MY_ASM/JOGENSEN_x86/stdin.asm
+++ MY_ASM/JOGENSEN_x86/stdin.asm
@@ -1,4 +1,37 @@
 section	.data
 
-STDIN:		
+STDIN:		equ	0
+SYS_read	equ	0
 inChar:		db	0x0
+
+
+
+
+section	.text
+
+global _start
+
+_start:
+	nop
+	nop
+	nop
+	jmp	read
+
+read:	
+	mov	eax,0
+	mov	rdi,0
+	mov	rsi,inChar
+	mov	rdx,1
+	syscall
+
+write:
+	mov	rax,1
+	mov	rdi,1
+	mov	rsi,inChar
+	mov	rdx,1
+	syscall
+
+exit:
+	mov	rax,60
+	xor	rdi,rdi
+	syscall
