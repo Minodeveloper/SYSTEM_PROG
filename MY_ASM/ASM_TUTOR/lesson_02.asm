b1f35073a616f5c6b0df278b34e10f7fc9343b5d
Minodeveloper <minoshideveloper8@gmail.com>
2023-04-15 19:48:52 +0530

    progress

---
+++ MY_ASM/ASM_TUTOR/lesson_02.asm
@@ -0,0 +1,20 @@
+section	.data
+
+msg:		db	"Hello World!",0xa
+len:		equ	($-msg)
+section	.text
+
+global	_start
+
+_start:
+	mov	edx,len
+	mov	ecx,msg
+	mov	ebx,1
+	mov	eax,4
+	int	0x80
+
+
+exit:
+	mov	eax,1
+	mov	ebx,0
+	int	0x80
