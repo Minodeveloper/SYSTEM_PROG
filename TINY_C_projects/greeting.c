8e6645745db3366db72f44b5943259d7f600925d
Minodeveloper <minoshideveloper8@gmail.com>
2023-06-11 22:18:19 +0530

    understood times

--- TINY_C_projects/greeting.c
+++ TINY_C_projects/greeting.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-
+#include <time.h>
 int main(int argc, char *argv[])
 {
-	printf("HElLO WORLD\n");
+	time_t now;
+	time(&now);
+
+	printf("The computer thinls it's %ld\n", now);
+
+	printf("%s\n", ctime(&now));
+
+	if (argc < 2)
+	{
+		printf("Hello , you handsome beast !\n");
+	}
+	else
+	{
+		printf("Hello, %s !\n", argv[1]);
+	}
 
 	return 0;
 }
