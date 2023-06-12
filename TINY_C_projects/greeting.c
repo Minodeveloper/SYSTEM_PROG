4ab8d895e01ba4752fa90a0fcd17966c2f9856a1
Minodeveloper <minoshideveloper8@gmail.com>
2023-06-12 18:00:22 +0530

    welcome script completred

--- TINY_C_projects/greeting.c
+++ TINY_C_projects/greeting.c
@@ -4,21 +4,41 @@ int main(int argc, char *argv[])
 {
 	time_t now;
 
-	time(&now);
+	now = time(NULL);
 
-	printf("%ld noow \n", now);
+	struct tm *clock;
 
-	printf(time(&now));
+	clock = localtime(&now);
 
-//	printf("\t\tHELLO MASTER !\n");
+	int hour = clock->tm_hour;
 
-//	puts("");
+	printf(" \t\t");
+	if(hour < 6)
+	{
+		printf("MASTER %s YOU NEED TO SLEEP NOW !\n",argv[1]);
+	}else if (hour > 6 && hour <  12)
+	{
+		printf("GOOD MORNING MASTER !\n");
+	}
+	else if (hour > 12 && hour < 16)
+	{
+		printf("GOOD AFTERNOON MASTER %s!\n",argv[1]);
 
-	
+	}
+	else if (hour > 16 && hour < 22)
+	{
+		printf("GOOD EVENING MASTER !\n");
+	}
+	else 
+	{
+		printf("TIME TO SLEEP MASTER !\n");
+
+	}
+
+	printf("\n");	
+	printf("\t\t%s \n", ctime(&now));
 
 
-	
-	return 0;
-
-	
+		
+	return 0;	
 }
